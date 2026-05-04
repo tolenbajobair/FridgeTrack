0447af1d817cf9a69ac0be292488b92af2f3b850
tolenbajobair <tolen.bajobair@gmail.com>
2026-05-04 19:50:09 +0300

    Update db.js

--- JS files/db.js
+++ JS files/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "fridgetrack",
+    host: process.env.MYSQLHOST || "tramway.proxy.rlwy.net",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "GxBORcmQGsZVCbcdXaaKLZoGBkeIJTEb",
+    database: process.env.MYSQLDATABASE || "railway",
+    port: process.env.MYSQLPORT || 36359,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
